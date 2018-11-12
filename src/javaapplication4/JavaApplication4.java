8bef170f09469e892447a77bfb5390aea69e854e
Labeeba Begum <g00064828@aus.edu>
2018-11-12 14:50:50 +0400

    feature added by member b

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -14,8 +14,18 @@ public class JavaApplication4 {
     /**
      * @param args the command line arguments
      */
+    
+    static public void featureB()
+    {
+        System.out.println("Feature B added");
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        featureB();
     }
+    
+    
     
 }
